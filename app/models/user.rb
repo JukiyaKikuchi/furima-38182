aee84e752e70463d63a148ad38d08563f6e0d9ea
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2022-10-10 11:36:19 +0900

    新規登録・ログイン画面実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :nickname, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  validates :birthday, presence: true
 end
