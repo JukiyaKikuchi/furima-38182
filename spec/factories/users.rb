e044c37bcd859af4d4c0d41139aceba6f9a0ba07
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2022-10-14 11:16:24 +0900

    passwordに半角英数字が少なくとも１字含む実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname        {Faker::Name.name}
     email           {Faker::Internet.free_email}
-    password        {Faker::Internet.password(min_length: 6)}
+    password        {"g9"+Faker::Internet.password(min_length: 6)}
     password_confirmation      {password}
     last_name       {"テスト"}
     first_name      {"テスト"}
