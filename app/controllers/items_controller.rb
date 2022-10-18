228d8f69227af50fe9410cfcbf9c93323facf4bd
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2022-10-18 12:19:07 +0900

    ActiveHash導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :item_description, :item_price, :image, :category_id, :item_status_id, :delivery_date_id, :delivery_cost_id, :prefecture_id).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  validates :item_name, :item_description, :item_price, presence: true
+
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :delivery_cost
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :delivery_date
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_status
+
+  validates :category_id, :item_status_id, :delivery_cost_id, :delivery_date_id, :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
 end
