0627a69808b04c166a0ae9d1ebb1809ae17861fd
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2022-10-23 07:12:32 +0900

    round→floor

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -3,9 +3,9 @@ function price (){
   input.addEventListener("input", () => {
     const inputValue = input.value;
     const addTax = document.getElementById("add-tax-price")
-    addTax.innerHTML = Math.round(inputValue * 0.1);
+    addTax.innerHTML = Math.floor(inputValue * 0.1);
     const profit = document.getElementById("profit");
-    profit.innerHTML = Math.round(inputValue - Math.round(inputValue * 0.1));    
+    profit.innerHTML = Math.floor(inputValue - Math.round(inputValue * 0.1));    
   })
 };
 
