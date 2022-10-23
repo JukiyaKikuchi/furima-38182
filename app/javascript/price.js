b2b2b3eb390e56ab4a974e5ae8910fcc8663d7dc
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2022-10-23 15:37:43 +0900

    再レビュー後修正（Math.round→Math.floor)

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -5,7 +5,7 @@ function price (){
     const addTax = document.getElementById("add-tax-price")
     addTax.innerHTML = Math.floor(inputValue * 0.1);
     const profit = document.getElementById("profit");
-    profit.innerHTML = Math.floor(inputValue - Math.round(inputValue * 0.1));    
+    profit.innerHTML = Math.floor(inputValue - Math.floor(inputValue * 0.1));    
   })
 };
 
