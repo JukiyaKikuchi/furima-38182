f30d622864051e9e0ee407e5f021c8912b9277c7
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2022-10-26 11:19:58 +0900

    コントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_item, only: [:edit, :update]
+  before_action :corect_user, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -22,9 +24,27 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:item_name, :item_description, :item_price, :image, :category_id, :item_status_id, :delivery_date_id, :delivery_cost_id, :prefecture_id).merge(user_id: current_user.id)
   end
-
+  def find_item
+    @item = Item.find(params[:id])
+  end
+  def corect_user
+    @item = Item.find(params[:id])
+    @user = @item.user
+    redirect_to(items_path) unless @user == current_user
+  end
 end
