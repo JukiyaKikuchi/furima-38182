f36a096be1e4f56f4213a092b05560bdf0d73c93
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2022-11-04 18:49:28 +0900

    モデル編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,9 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+  has_one :order
+
+
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -1,9 +1,9 @@
 class OrderDelivery
   include ActiveModel::Model
-  attr_accessor :postcode, :city, :address, :building, :tell_number, :prefecture_id, :user_id, :item_id
+  attr_accessor :postcode, :city, :address, :building, :tell_number, :prefecture_id, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :user_id, :item_id, :city, :address
+    validates :user_id, :item_id, :city, :address, :token
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :tell_number, format: {with:/\A\d{10,11}\z/}, length: {maximum: 11}
   end
@@ -11,6 +11,6 @@ class OrderDelivery
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
-    derivery.create(postcode: postcode, city: city, address: address, building: building, tell_number: tell_number, prefecture_id: prefecture_id, order_id: order.id)
+    Delivery.create(postcode: postcode, city: city, address: address, building: building, tell_number: tell_number, prefecture_id: prefecture_id, order_id: order.id)
   end
 end
