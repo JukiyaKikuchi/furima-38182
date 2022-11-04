a7cf1df6d43adda684abaa248dd8ab2530f1c9ec
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2022-11-04 18:47:12 +0900

    テストコード作成

--- spec/factories/order_delivery.rb
+++ spec/factories/order_delivery.rb
@@ -6,5 +6,6 @@ FactoryBot.define do
     address           { '1-1' }
     building          { 'ヤマダ' }
     tell_number       { 22222222222 } 
+    token             { "tok_abcdefghijk00000000000000000" }
   end
 end
